8c1c230d1c02b2bf4fcde72b6ef5aa079e7dbfcd
victordlt <v.delatorreda@gmail.com>
2022-05-14 13:11:40 +0200

    db_tarjeta

--- db_tarjeta.js
+++ db_tarjeta.js
@@ -1,11 +1,27 @@
 module.exports=[{
     "nro_tarjeta": 4546857418565565,
     "clave": 4345,
-    "saldo": 3636.00,
+    "saldo": 40555.00,
     "id_cliente":1
   }, {
     "nro_tarjeta": 5595345899897125,
     "clave": 1595,
-    "saldo": 20.00,
+    "saldo": 3566.00,
     "id_cliente":2
-  }]
+  }, {
+    "nro_tarjeta":4858669658871578,
+    "clave":1234,
+    "saldo":23.00,
+    "id_cliente":3
+  },{
+    "nro_tarjeta":5854665625871547,
+    "clave":4345,
+    "saldo":300.00,
+    "id_cliente":4
+  },{
+    "nro_tarjeta":4546989623571478,
+    "clave":0023,
+    "saldo":35621,
+    "id_cliente":5
+  }
+]
