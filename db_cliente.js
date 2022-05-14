a352015b513681f3473cd0a5457b6eb3b7eb8820
victordlt <v.delatorreda@gmail.com>
2022-05-14 13:09:37 +0200

    db_cliente

--- db_cliente.js
+++ db_cliente.js
@@ -16,4 +16,35 @@ module.exports=[{
     "dni": 45879854,
     "email": "victor@gmail.com",
     "direccion": "calle cuzco 500"
-  }]
+  },{
+    "id_cliente":3,
+    "nombre": "German",
+    "apellido_paterno":"Walton",
+    "apellido_materno":"Perez",
+    "telefono":548796533,
+    "dni":74658951,
+    "email":"german@gmail.com",
+    "direccion":"calle bancario 350" 
+  },{
+    "id_cliente":4,
+    "nombre":"Cintia",
+    "apellido_paterno":"Beltramen",
+    "apellido_materno":"Bellido",
+    "telefono":687945812,
+    "dni":64589732,
+    "email":"cintia@gmail.com",
+    "direccion":"av los angeles 200"
+  },{
+    "id_cliente":5,
+    "nombre":"Facundo",
+    "apellido_paterno":"Alastra",
+    "apellido_materno":"Torres",
+    "telefono":405165871,
+    "dni":46958887,
+    "email":"facundo@gmail.com",
+    "direccion":"calle orquideas 420"
+  }
+
+
+
+]
