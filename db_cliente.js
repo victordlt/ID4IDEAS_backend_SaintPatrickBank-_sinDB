6c0b4d4c2270c60ae28bc78d2c86e72a0cdab0c2
victordlt <v.delatorreda@gmail.com>
2022-05-14 12:54:02 +0200

    app, db_cliente, db_historial_transaccion

---
+++ db_cliente.js
@@ -0,0 +1,19 @@
+module.exports=[{
+    "id_cliente": 1,
+    "nombre": "Isabel",
+    "apellido_paterno": "Torres",
+    "apellido_materno": "Trevinos" ,
+    "telefono": 689546872,
+    "dni": 65489752,
+    "email": "isabel@gmail.com",
+    "direccion": "calle amazonas 400"
+  }, {
+    "id_cliente": 2,
+    "nombre": "Victor",
+    "apellido_paterno": "De La Torre",
+    "apellido_materno": "de Alarcon" ,
+    "telefono": 659485786,
+    "dni": 45879854,
+    "email": "victor@gmail.com",
+    "direccion": "calle cuzco 500"
+  }]
---
+++ db_historial_transaccion.js
@@ -0,0 +1,17 @@
+module.exports=[
+  //{"seqValue":2},
+  {
+    //"id_transaccion": 1,
+    "operacion": "ingreso",
+    "monto": 15.00,
+    "fecha": "2021-12-27" ,
+    "nro_tarjeta_propia": 4546857418565565,
+    "nro_tarjeta_ajena": 5595345899897125
+  }, {
+    //"id_transaccion": 2,
+    "operacion": "retiro",
+    "monto": 12.00,
+    "fecha": "2021-12-30" ,
+    "nro_tarjeta_propia": 4858669658871578,
+    "nro_tarjeta_ajena": 4546857418565565
+  }]
